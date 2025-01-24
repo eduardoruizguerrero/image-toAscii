7c7ed27a03c7009fa07458724f52c327069e8b92
Eduardo Ruiz Guerrero <157335271+eduardoruizguerrero@users.noreply.github.com>
2025-01-24 18:15:08 -0300

    minor changes

--- ascii.js
+++ ascii.js
@@ -1,94 +1,156 @@
 /*
-*   Carga la imagen, la convierte a una URL Base64 y la guarda en el local storage.
+
+
+
+
+                    *****************************************************
+                    *   This code is made from Eduardo Ruiz Guerrero.   *
+                    *****************************************************
+
+
+
+
+        %%%%%%%%%%%%%%%%%@@@%%%%%%%%%%%%@%%*%%%@@%%=-=*%%%%%%%%%%%%*********************
+        %%%%%%%%%%%%%%%%@@@@%%%%%%%%%%%%@%%**%%@@@%%=::-*%%%%*%%%%%%%**+++*******%%*****
+        %%%%%%%%%%%%%%%%@@@%%%%%%%%%%%%%*%%%*%%%@@%%%=:.:-*%%%%*%%%%%%%*+=+++++*********
+        %%%%%%%%%%%%%%%@@@@%%%%%%%%%%%%%=*%%%*%%@@@%%%=:..:=*%%%**%%%%%%%*++++++=++++++=
+        %%%%%%%%%%%%%%%@@@@%%%%%%%%%%%%+-=%%%*%%%@%=%%%=:.:::+%%*%%%%%%%%%%+=++++++=====
+        %%%%%%%%%%%%%%%@@@@%%%%%%%%%%%*=::%%%%%%%%%:+%%*-::..:-*%=*%%%%%%%%%+===========
+        %%%%%%%%%%%%%%%@@@@%%%%%%%%%%%=:.:+%%%%%%%%::+%%=-....::+%-=%%%%%@@%%+==========
+        %%%%%%%%%%%%%%%%@@@%%%%%%%%%%+--==+%%%%%%%%-.:+%+-:....::=*--*%%%%@@%%**++++====
+        %%%@%%%%%%%%%%%%@@@%%%%%%%%%+-::..:*%%%%%%%::.:**:-:.....:-*:=+%%%%@@%%++****+++
+        %%%@%%%%%%%%%%%%@@@%%%%%%%%*:..::..+%%%%%%*:...:*=::.......-*::=%%%%@@%+====+++*
+        %%%@%%%%%%%%%%%%%@@%%%%%%%%-.......-%%%%%%+:....=+:-:......:==..=%%%@@%*====-==+
+        %%%@%%%%%%%%%%%%%@@%%%%%%%=.:.....::%%%%%%=.....:*:::........=-::-%%@@@%+===-==+
+        %%%@%%%%%%%%%%%%%%@@%%%%%*::......::*+*%%*:......=-:-.......:-==*%%%%@@%*====-==
+        %%%%@%%%%%%%%%%%%%@@%%%%%-......::::+=-%%+:.:....:=.-:......:-%@%*%=*@@@%=-==-:-
+        %%%%@%%%%%%%%%%%%%%@%%%%=::.....::::==-*%=-::....:=.-:......:*%@%=+:=*@@*=======
+        %%%%%%%%%%%%%%%%%%%%%%%+:.::-=+*%%%@@%**+:::::....-:::......:*:%@-=.-==%+==-====
+        %%@%%@%%%%%%%%%%%%%%@%*::+%@@@@@@@@@%%%@@%+:::....::........:%*@@-:.-===+=------
+        %%@%%%%%%%%%%%%%%%%%%%-+@@@@@%**%%@@@@%+=+**:..............:-@@@@:..====-=------
+        %%%%%%@%%%%%%%%%%%%%%%%@@@@%+=+*:.:%@@@@=:--:..............:-@%**..:=-=---------
+        %:.=%%%%%%%%%%%%%%%%%%@@@@%===@-...%@%@@%:.:................:%%+:.::+-=-==------
+        +..:*%%%%%%%%%%%%%%%%%%*@@*=-+@*=-+@@@@@@+...................:==::::*========---
+        *:::-%%%%%%@%%%%%%%%%%%*=@%=:*@@@@@@@@@@@*..:....................:.:*==++====---
+        %::-:+%%%@%%@%%%%%%%%%%%=-%+.=@@@@@@@@%%@*......................::.:*+=+=++=====
+        @+:---*%%%@%@@%%%%%%%%%%%::=-.*@@@%*++++=-.......................:.:************
+        @@=:..-%%%%%%@%%%%%%%%%%%*::::.+@@%++++**...:................:....:++*****++++==
+        %@@=:.:=%%%%@@@@@@%%%%%%%%*::...:+%%%%*=:..::...............::...:=%=+++++++++==
+        %%%@*-::+%%%%@@@@@@@@%%%%%%+:.......:---::......................:+%*++++++++++++
+        %%%%@@%+:*%%%%@@@@@@@@@@%%%%+:::......::::....................:-*@%**********+**
+        %%%%%@@@%%@%%%%@@@@@@@@@+:-+*+:.......:.:::..................:+%@%%%****++*%*+**
+        %%%%%%@@@%%@%%%%@@@@@@@@%=:.:--:.........::...........:-=:.:-%@@@%@%*****+**++++
+        %%%%%%%@@%%%@%%%%@@@@@@@%%=::........................:=-:.:+@@@@%%%%*****+++=---
+        *%%%%%%%@@%%%@%%%%%@@@@@@%%+:.......................:.:..=%@@@@@%%%%%****++-----
+        *%@%%%%%%@%%@@@%%%%%@@@@@@%%*=-:::..:..................:+%@@@@@%%%%%%%%%%*=:==--
+        **@%%%%%%@@%@@@@@%%%%%@@@@@%%%++++=---::::::::::::::-=*@@%@@@@%%%%%%%%%%%=:-----
+        %%@%%%%%%%@@%%%@@@%%%%%@@@@@%%%*==+++++=========+++%@@@@@@@@%%%%%%%%%%%*=--=----
+        %%@%@%%%%%@%%%%%%@@%%%%%%@@@@%%%*+=====+++++++++***%%%%%%@@%@%%%%%%%%%*---------
+        %%@%%%%%%%@%%%%%%%%%%%%%@%%@@@%%%%+==========+*%%%%%%*=%%%%%%%%++**%**---====---
+        *%%%%%%%%%%%%%%%%%%%%%%%%@%%%@@%%%%*+=======+**%%%**++=%%@%%%%*=**%%+=*%%%%%%%%%
+        +*%%%%%%%@%%%%%%%%%%%%%%%%%%%%%%%%%%%*+====++=****===++%%@%%%*++***=*%@@@@@@@@@@
+        %*%%%%%%%@%%%%%%%%%%%%%%%%%%@%%%%%%%%%@%**++=-+==+=+=*%%%%%*+*+***=+%%=---------
+
+
+
+
 */
 
-/*
-*   Permitir poder cambiar la imagen que subimos a la web.
-*   Crear la constante lector.
-*   Cargar la imagen al local storage con el nombre "recent-image"
-*   y renderizarla.
-*/
-document.getElementById('subirFoto').addEventListener('change', function(e) {
-    const canvas = document.getElementById('micanvas');
-    const ctx = canvas.getContext('2d');
+
+
+
+let chars = "#@%*+=-:. ";
+let imageData, imageWidth, imageHeight;
+
+//  Subir imagen.
+document.getElementById("uploadinput").addEventListener("change", function (e) {
+    /*
+        Toma el contexto (la imagen cargada) y la transforma a 2d, eje x & eje y.
+        Crea un lector de archivos.
+    */
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d", { willReadFrequently: true });
     const reader = new FileReader();
-    /*
-    *   Carga la imagen al local storage con el nombre "recent-image".
-    */
-    reader.addEventListener("load", () =>(
-        localStorage.setItem("recent-image", reader.result)        
-    ));
+    
+    //  Cargar archivo
+    reader.onload = () => {
+        //  Crear la imagen.
+        const imagen = new Image();
+        imagen.src = reader.result;
+    
+        //  Cargar la reciente imagen.
+        imagen.onload = () => {
+            const scale = 2;
+            //  Crear un canvas temporal para escalar la imagen.
+            const TemporalCanvas = document.createElement("canvas");
+            const TemporalCtx = TemporalCanvas.getContext("2d", { willReadFrequently: true });
+            TemporalCanvas.style.display = "none";
+            
+            TemporalCanvas.width = imagen.width * scale;
+            TemporalCanvas.height = imagen.height * scale;
+            TemporalCtx.drawImage(imagen, 0, 0, TemporalCanvas.width, TemporalCanvas.height);
+    
+            //  Asignar las características de la imagen escalada al canvas.
+            canvas.width = TemporalCanvas.width;
+            canvas.height = TemporalCanvas.height;
+    
+            //  Dibujar la imagen con sus datos.
+            ctx.drawImage(TemporalCanvas, 0, 0);
+            imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            imageWidth = canvas.width;
+            imageHeight = canvas.height;
+                
+            updateAscii();
+        };
+    };
+    
     reader.readAsDataURL(this.files[0]);
-    /*
-    *   let canvas indica donde queremos dibujar nuestro lienzo,
-    *   ctx da contexto sobre que vision queremos del lienzo
-    *   una 2d o 3d.
-    */
-    canvas.width = imagen.width;
-    canvas.height = imagen.height;
-    ctx.drawImage(imagen, 0, 0);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-    const asciiStr = imageToAscii(data, canvas.width, canvas.height);
-    document.getElementById('mipre').textContent = asciiStr;
 });
-/*
-*   Ya definida la constante recentImageDataUrl1
-*   el script cambia la ruta de acceso a esa foto subida,
-*   por la de local storage,
-*   en otras palabras, actualiza nuestra nueva foto.
-*/
-const imagen = new Image();
-document.addEventListener("DOMContentLoaded", ()=>{
-    const recentImageDataUrl1 = localStorage.getItem("recent-image");
-    if (recentImageDataUrl1){
-        document.querySelector("#img").setAttribute("src", recentImageDataUrl1);
-        document.querySelector("#img").setAttribute("hidden","true")
-        imagen.setAttribute("src", recentImageDataUrl1);
+    
+function updateAscii() {
+    if (!imageData) return;
+    
+    //  Definir el ancho y alto de la imagen ascii dependiendo de que imagen subimos.
+    let sizeWidth, sizeHeight;
+    if (imageWidth < 500){
+        sizeWidth = 5;
+        sizeHeight = 10;
     }
-    imagen.src = recentImageDataUrl1;
-})
+    if (imageWidth >= 500){
+        sizeWidth = 10;
+        sizeHeight = 20;
+    }
+    if (imageWidth >= 1000){
+        sizeWidth = 25;
+        sizeHeight = 50;
+    }
+    
+    const asciiString = imageToAscii(imageData.data, imageWidth, imageHeight, sizeWidth, sizeHeight);
+    document.getElementById("pre").textContent = asciiString;
+}
 
 /*
-*   Crea el formato Ascii.
+    Con los datos necesarios, representar la imagen en formato ascii,
+    definiendo los chars utilizados,
+    el ancho y alto de la imagen final,
+    los niveles de iluminación,
+    y la escala de grises a utilizar.
 */
+function imageToAscii(data, width, height, sizeWidth, sizeHeight) {
+    let asciiJump = "";
 
-function imageToAscii(data, width, height) {
     /*
-    *   let chars representa los símbolos que usará cada escala de gris.
+        Definir variables y e x, en búcles for que recorran la imagen,
+        y sean de menor tamaño que el ancho del canvas (la imagen).
     */
-    //const chars = '#&%$!mnusa+=-_,.'/*valores originales*/
-    const chars = '@#$mnusa+=-.'
-    //const chars = '█▀▄anvcxzoue=+'//movil
-    //const chars = '█▀▄nuza=+'//movil
-    //const chars = 'anvcxzoue=+' /*valores para dispositivos móviles*/
-    /*
-    *   let asciiJump es el salto de línea.
-    */
-    let asciiJump = '';
-    /*
-    *   const size define el ancho y el alto
-    *   de la representación de pre.
-    */
-    const sizeWidth = 10;//30 movil
-    const sizeHeight = 20;//60 movil
-    /*
-    *   El búcle for recorrerá los pixeles del eje Y y del eje X.
-    */
-    for (let y = 0; y < height; y += sizeHeight) {
+    for (let y = 0; y < height; y += sizeHeight){
         for (let x = 0; x < width; x += sizeWidth) {
             let graySum = 0;
-            /*
-            *   let pixeles define cuantos pixeles interpretará el script.
-            */
             let pixeles = 0;
-            /*
-            *   El búcle for recorerrá el eje X y el eje Y
-            *   en busca de la escala de grises del pixel.
-            */
-            for (let sizey = 0; sizey < sizeHeight; sizey++) {
-                for (let sizex = 0; sizex < sizeWidth; sizex++) {
-                    if (x + sizex < width && y + sizey < height) {
+
+            for (let sizey = 0; sizey < sizeHeight; sizey++){
+                for (let sizex = 0; sizex < sizeWidth; sizex++){
+                    if (x + sizex < width && y + sizey < height){
                         const offset = ((y + sizey) * width + (x + sizex)) * 4;
                         const r = data[offset];
                         const g = data[offset + 1];
@@ -99,24 +161,63 @@ function imageToAscii(data, width, height) {
                     }
                 }
             }
+
             const grayAvg = graySum / pixeles;
             const charIndex = Math.floor((grayAvg / 255) * (chars.length - 1));
             asciiJump += chars[charIndex];
         }
+
         asciiJump += '\n';
     }
+
     return asciiJump;
 }
+
+
+
 /*
-*   Botón para copiar el contenido de <pre>.
+
+    Botones.
+
 */
-/*
-function copiarAscii(f){
-    var temporalLet = document.createElement("input");
-    document.getElementsByTagName("body")[0].appendChild(temporalLet);
-    temporalLet.value = f.innerHTML;
-    temporalLet.select();
-    document.execCommand("copy");
-    document.getElementsByTagName("body")[0].removeChild(temporalLet);
-}
-*/
+
+//  Limpiar el canvas, el pre y el input file.
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+    const uploadinput = document.getElementById("uploadinput");
+    const pre = document.getElementById("pre");
+    const canvas = document.getElementById("canvas");
+    
+    uploadinput.value = "";
+    canvas.width = 0;
+    canvas.height = 0;
+    pre.textContent = "";
+});
+
+//  Copiar el texto de pre.
+const copy = document.getElementById("copy");
+copy.addEventListener("click", () => {
+    const pre = document.getElementById("pre").innerHTML;
+    navigator.clipboard.writeText(pre);
+    
+    const text = document.getElementById("pre")
+    const rango = document.createRange();
+    rango.selectNodeContents(text);
+    const seleccion = window.getSelection();
+    seleccion.removeAllRanges();
+    seleccion.addRange(rango);
+});
+
+//  Cambiar los chars al mode one
+const modeone = document.getElementById("modeone");
+modeone.addEventListener("click", () => {
+    chars = "#@%*+=-:. ";
+    if (imageData) updateAscii();
+});
+
+//  Cambiar los chars al mode two
+const modetwo = document.getElementById("modetwo");
+modetwo.addEventListener("click", () => {
+    chars = "█▓▒░ ";
+    if (imageData) updateAscii();
+});

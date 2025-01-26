96acd57c465a648bffcb17c26221f756f8efd95d
Eduardo Ruiz Guerrero <157335271+eduardoruizguerrero@users.noreply.github.com>
2025-01-26 20:06:33 -0300

    added download button functionality

--- ascii.js
+++ ascii.js
@@ -157,7 +157,8 @@ function imageToAscii(data, width, height, sizeWidth, sizeHeight) {
                         const r = data[offset];
                         const g = data[offset + 1];
                         const b = data[offset + 2];
-                        const gray = 0.3 * r + 0.59 * g + 0.11 * b;
+                        //const gray = 0.3 * r + 0.59 * g + 0.11 * b;   //  escala original, si aumentás los parámetros es más claro, caso contrario más oscuro.
+                        const gray = 0.29 * r + 0.58 * g + 0.10 * b;
                         graySum += gray;
                         pixeles++;
                     }
@@ -212,20 +213,61 @@ copy.addEventListener("click", () => {
     seleccion.addRange(rango);
 });
 
-//  Cambiar los chars al mode one
+//  Cambiar los chars al mode one.
 const modeone = document.getElementById("modeone");
 modeone.addEventListener("click", () => {
     chars = "#@%*+=-:. ";
     if (imageData) updateAscii();
 });
 
-//  Cambiar los chars al mode two
+//  Cambiar los chars al mode two.
 const modetwo = document.getElementById("modetwo");
 modetwo.addEventListener("click", () => {
     chars = "█▓▒░ ";
     if (imageData) updateAscii();
 });
 
+//  Descargar la imagen ascii.
+const download = document.getElementById("download");
+download.addEventListener("click", () => {
+    const pre = document.getElementById("pre");
+    const style = getComputedStyle(pre);
+    const lines = pre.textContent.split("\n");
+    const lineHeight = parseInt(style.lineHeight) || 15;
+    const padding = 2;
+    const fontSize = parseInt(style.fontSize)
+    const fontFamily = style.fontFamily;
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const width = pre.offsetWidth + padding * 2;
+    const height = lines.length * lineHeight - 15;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.fillStyle = style.backgroundColor || "#000";
+    ctx.fillRect (0, 0, canvas.width, canvas.height);
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    ctx.fillStyle = style.color || "#fff";
+    ctx.textBaseline = "top";
+
+    lines.forEach((line, i) => {
+        ctx.fillText(line, padding, padding + i * lineHeight);
+    });
+
+    //  Mantener nombre orginal y agregar "-ascii.png".
+    const inputfile = document.getElementById("uploadinput");
+    const archivo = inputfile.files[0];
+    const nombre = archivo.name.split('.').slice(0, -1).join('.');
+
+    const link = document.createElement("a");
+    link.download = `${nombre}-ascii.png`;
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+})
+
+//  Habilitar o deshabilitar los botones en función de si hay o no hay una imagen ascii creada.
 const ascii = document.getElementById("pre");
 function updateButtonStates() {
     const avaiblecontent = ascii.textContent !== "";
@@ -233,6 +275,7 @@ function updateButtonStates() {
     copy.disabled = !avaiblecontent;
     modeone.disabled = !avaiblecontent;
     modetwo.disabled = !avaiblecontent;
+    download.disabled = !avaiblecontent;
 }
 
 updateButtonStates();
